083755e8290dbac52b313b4afa227d83c043d26f
betosousa <avss@cin.ufpe.br>
2017-06-14 09:51:24 -0300

    Ajustes no invoker

--- src/infrastructure/ClientRequestHandlerReliable.java
+++ src/infrastructure/ClientRequestHandlerReliable.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
--- src/infrastructure/ServerRequestHandlerReliable.java
+++ src/infrastructure/ServerRequestHandlerReliable.java
@@ -9,12 +9,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 /**
